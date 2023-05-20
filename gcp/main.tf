8281e7217eb6e5d5b6ff3094e74a83f68bbbdd50
yogi8 <yogeeswarreddyp@gmail.com>
2023-05-20 15:20:12 +0530

    initial

--- gcp/main.tf
+++ gcp/main.tf
@@ -3,7 +3,7 @@ resource "google_container_cluster" "cluster" {
   location = var.region
 
   remove_default_node_pool = true
-  initial_node_count       = 3
+  initial_node_count       = 1
 }
 resource "google_container_node_pool" "nodes" {
   name       = "${var.name}-nodes"
